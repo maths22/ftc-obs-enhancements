bb4550c877b6510e3485bae2cc902adc5e9d679a
Jacob Burroughs <jburroughs@instructure.com>
2023-07-14 10:42:42 -0500

    Add cri lower third

---
+++ cri_lower_third/script.js
@@ -0,0 +1,44 @@
+
+
+function handleResize() {
+  const xZoom = window.innerWidth / 1920;
+  const yZoom = window.innerHeight / 1080;
+  const displayScale = Math.min(xZoom, yZoom);
+
+  // Set the value of variable --blue to another value (in this case "lightblue")
+  document.body.style.setProperty('--display-scale', displayScale);
+}
+
+window.addEventListener('resize', handleResize)
+
+handleResize()
+
+function toggle(remove, add) {
+  const star = document.getElementById("star");
+  const background = document.getElementById("text-background");
+
+  star.classList.remove(`star-${remove}`)
+  background.classList.remove(`text-background-${remove}`)
+  // Force style recalc
+  star.offsetHeight
+  star.classList.add(`star-${add}`)
+  background.classList.add(`text-background-${add}`)
+}
+
+function show() {
+  toggle('out', 'in')
+}
+
+function hide() {
+  toggle('in', 'out')
+}
+
+window.addEventListener('show-pp-third', function(event) {
+  document.getElementById("line-1").textContent = event.detail.line_1
+  document.getElementById("line-2").textContent = event.detail.line_2
+  show()
+})
+
+window.addEventListener('hide-pp-third', function(event) {
+  hide()
+})
